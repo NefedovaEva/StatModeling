d061a6615111c019ca953d24cb435623b601be65
unknown <konek.ekb@gmail.com>
2024-06-09 17:21:21 +0300

    Upload task 2.2, 2.33

--- cube.py
+++ cube.py
@@ -1,18 +1,13 @@
-from turtle import lt
-
 import numpy as np
-import pandas as pd
-import scipy as scipy
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
-
-# Set the number of points
 n_points = 30
 dimension = 100
 a = 0.5
+
 # Generate random points inside the cube [-0.5, 0.5]^100
 points = np.random.uniform(-a, a, size=(n_points, dimension))
+
 # Calculate pairwise distances between points
 distances = np.zeros((n_points, n_points))
 for i in range(n_points):
@@ -35,9 +30,19 @@ for i in range(n_points):
       n_pairs += 1
 pairs = np.arange(1, n_pairs+1)
 plt.scatter(pairs, distance)
-plt.grid ( True )
-plt.xlabel('Пара точек') #Подпись для оси х
-plt.ylabel('Расстояние') #Подпись для оси y
-plt.title('Расстояние между точками') #Название
+plt.grid(True)
+plt.xlabel('Pair of points')
+plt.ylabel('Distance')
+plt.show()
+
+angle=[]
+for i in range(n_points):
+    for j in range(i + 1, n_points):
+      angle.append(angles[i, j])
+pairs = np.arange(1, n_pairs+1)
+plt.scatter(pairs, angle)
+plt.grid(True)
+plt.xlabel('Pair of points')
+plt.ylabel('Angle')
 plt.show()
 
---
+++ d3.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+n_points = 100
+dimension = 3
+
+# Generate random points on the surface of a sphere
+random_points = np.random.normal(0, 1, size=(n_points, dimension))
+norm_point = np.zeros(n_points)
+points = np.zeros((n_points, dimension))
+for i in range(n_points):
+    norm_point[i] = np.linalg.norm(random_points[i])
+for i in range(n_points):
+    for j in range(dimension):
+      points[i, j] = random_points[i, j]/norm_point[i]
+
+# Calculate pairwise distances between points
+distances = np.zeros((n_points, n_points))
+for i in range(n_points):
+    for j in range(n_points):
+     distances[i, j] = np.linalg.norm(np.subtract(points[i], points[j]))
+distance = []
+for i in range(n_points):
+    for j in range(i + 1, n_points):
+      distance.append(distances[i, j])
+print(points)
+
+# Plotting the histogram
+plt.hist(distance, bins=10, color='skyblue', edgecolor='black')
+plt.xlabel('Distance')
+plt.ylabel('Frequency')
+plt.grid(axis='y', alpha=0.75)
+plt.show()
